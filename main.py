f4decc48d9a28334c6a70c698bddc2d4b5cb39ca
Gary Chien <garychien1@gmail.com>
2016-11-08 16:04:42 -0500

    basic thresholding

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import cv2
 cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, img = cap.read()
-    cv2.imshow('input', img)
-    k = cv2.waitKey(10)
-    if k == 27:
-        break
 
+def get_next_frame():
+    img = None
+    if cap.isOpened():
+        _, img = cap.read()
+    return img
+
+def main():
+    while cv2.waitKey(10) != 27:
+        img = get_next_frame()
+        if img is None:
+            print 'No webcam detected!'
+            break
+        grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(grey, (5,5), 0)
+        ret, thresh1 = cv2.threshold(blur, 70, 255,
+                                     cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        cv2.imshow('input', thresh1)
+
+if __name__ == '__main__':
+    main()
